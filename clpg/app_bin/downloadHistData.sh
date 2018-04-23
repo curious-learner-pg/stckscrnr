13e8a01c7b8ab08a8ff86472390ce02e2b581641
curious-learner-pg <curious.learner.pg01@gmail.com>
2018-04-23 18:13:17 +0000

    Added code for new project - StckScrnr

---
+++ clpg/app_bin/downloadHistData.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+app_date=$(date '+%d%b%C%y' -d "+5hours30minutes" | tr '[:lower:]' '[:upper:]')
+export app_date
+
+app_path="/home/clpg"
+
+url='https://www.nseindia.com/content/historical/EQUITIES/2018/'
+
+cd $app_path/app_histData/
+
+for month in JAN FEB
+do 
+for i in $(cat ${app_path}/app_conf/${month}_working_days.list);
+    do
+        if [ -e $month.csv ]
+	then
+		echo "Data for $i available"
+	else
+		mon=$(echo $i | cut -c3-5)
+		suffix="bhav.csv"
+		tempurl="$url$mon/cm$i$suffix.zip"
+		file="cm$i$suffix"
+		echo "Pulling data for $i"
+		cd ./temp 
+		wget -U 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4' $tempurl
+		unzip $file.zip
+		grep -w -f ${app_path}/app_conf/stocks.list $file >> ${app_path}/app_histData/$month.raw
+		rm $file $file.zip
+	fi
+    done
+done
