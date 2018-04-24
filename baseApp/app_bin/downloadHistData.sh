5ad63daee94633c0771db97a07e9db5446b593e5
curious-learner-pg <curious.learner.pg01@gmail.com>
2018-04-24 10:47:50 +0000

    modified downloadHistData.sh to omit already loaded data

--- baseApp/app_bin/downloadHistData.sh
+++ baseApp/app_bin/downloadHistData.sh
@@ -7,26 +7,43 @@ app_path="/home/stckscrnr/baseApp"
 
 url='https://www.nseindia.com/content/historical/EQUITIES/2018/'
 
+get_data() {
+        mon=$(echo $1 | cut -c3-5)
+        suffix="bhav.csv"
+        tempurl="$url$mon/cm$1$suffix.zip"
+        file="cm$1$suffix"
+        echo "Pulling data for $1"
+        cd ${app_path}/app_histData/tmp
+        wget -U 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4' $tempurl
+        unzip $file.zip
+        grep -w -f ${app_path}/app_conf/stocks.list $file >> ${app_path}/app_histData/$2.raw
+        rm $file $file.zip
+
+        echo $1 >> ${app_path}/app_conf/working_days.done
+}
+
 cd $app_path/app_histData/
 
 for month in JAN FEB MAR
-do 
+do
 for i in $(cat ${app_path}/app_conf/${month}_working_days.list);
     do
-        if [ -e $month.raw ]
-	then
-		echo "Data for $month available"
-	else
-		mon=$(echo $i | cut -c3-5)
-		suffix="bhav.csv"
-		tempurl="$url$mon/cm$i$suffix.zip"
-		file="cm$i$suffix"
-		echo "Pulling data for $i"
-		cd ./temp 
-		wget -U 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4' $tempurl
-		unzip $file.zip
-		grep -w -f ${app_path}/app_conf/stocks.list $file >> ${app_path}/app_histData/$month.raw
-		rm $file $file.zip
-	fi
+        if [ -e ${app_path}/app_histData/$month.raw ]
+        then
+	    avail=$(grep -c $i ${app_path}/app_conf/working_days.done)
+	    if [ ${avail} -gt 0 ] 
+            then
+                echo "Data for $i available"
+	    else 
+		get_data $i $month
+            fi
+        else
+            get_data $i $month
+        fi
     done
 done
+
+
+
+sort ${app_path}/app_conf/working_days.done | uniq > ${app_path}/app_conf/working_days.chkdone
+mv ${app_path}/app_conf/working_days.chkdone ${app_path}/app_conf/working_days.done
