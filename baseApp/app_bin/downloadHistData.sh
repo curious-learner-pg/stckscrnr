2f57b86bc4d684e5a5a23b1ae1d96211d48b32d2
curious-learner-pg <curious.learner.pg01@gmail.com>
2018-04-24 12:14:40 +0000

    Added new list for 2017

--- baseApp/app_bin/downloadHistData.sh
+++ baseApp/app_bin/downloadHistData.sh
@@ -6,9 +6,11 @@ export app_date
 app_path="/home/stckscrnr/baseApp"
 
 url='https://www.nseindia.com/content/historical/EQUITIES/2018/'
+#url='https://www.nseindia.com/content/historical/EQUITIES/2017/'
 
 get_data() {
         mon=$(echo $1 | cut -c3-5)
+	yr=$(echo $1 | cut -c6-9)
         suffix="bhav.csv"
         tempurl="$url$mon/cm$1$suffix.zip"
         file="cm$1$suffix"
@@ -16,7 +18,7 @@ get_data() {
         cd ${app_path}/app_histData/tmp
         wget -U 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4' $tempurl
         unzip $file.zip
-        grep -w -f ${app_path}/app_conf/stocks.list $file >> ${app_path}/app_histData/$2.raw
+        grep -w -f ${app_path}/app_conf/stocks.list $file >> ${app_path}/app_histData/$2$yr.raw
         rm $file $file.zip
 
         echo $1 >> ${app_path}/app_conf/working_days.done
@@ -27,8 +29,9 @@ cd $app_path/app_histData/
 for month in JAN FEB MAR
 do
 for i in $(cat ${app_path}/app_conf/${month}_working_days.list);
+yr=17
     do
-        if [ -e ${app_path}/app_histData/$month.raw ]
+        if [ -e ${app_path}/app_histData/$month$yr.raw ]
         then
 	    avail=$(grep -c $i ${app_path}/app_conf/working_days.done)
 	    if [ ${avail} -gt 0 ] 
--- baseApp/app_bin/loadHistData2DB.sh
+++ baseApp/app_bin/loadHistData2DB.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 
+combine_data() {
+	
+}
+
+calc_size() {
+	s1=$()
+}
+
