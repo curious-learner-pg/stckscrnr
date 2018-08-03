4fc9a0bbff72e57868ddbcbe5fb909a2bc9ca5f2
curious-learner-pg <38314952+curious-learner-pg@users.noreply.github.com>
2018-08-03 15:50:49 +0530

    Added method to save data into CSV

--- baseApp/app_bin/getHistData.py
+++ baseApp/app_bin/getHistData.py
@@ -1,17 +1,19 @@
 import requests
 import re
 import time
+import csv
+
+histData = []
 
 def getHistData(tf, tp, symbol):
   baseurl = 'https://www.google.com/finance/getprices?'
   url = "i={0}&p={1}d&f=d,o,h,l,c,v&df=cpct&q={2}&x=NSE".format(tf,tp,symbol)
   ep_time_stamp = 0
-  histData = []
 
   req = requests.get(baseurl+url)
 
   if (req.status_code):
-    histDt = list(req.text.split('\n'))
+    histDt = list(req.text.splitlines())
 
     tf_offset = int((histDt[6].split('='))[1])
     print(tf_offset)
@@ -22,24 +24,28 @@ def getHistData(tf, tp, symbol):
       ep_ts = int(rec[0][1:len(rec[0])])+(tf_offset*60)
       print(ep_ts)
       ep_time_stamp = ep_ts
-      time_stamp = (time.asctime(time.localtime(ep_time_stamp)))
+      time_stamp = time.strftime("%d%m%y-%w-%H:%M:%S", (time.localtime(ep_time_stamp)))
       rec[0] = time_stamp
       #print(rec)
       histData.append(rec)
     else:
       rec = histDt[r].split(',')
-      time_stamp = (time.asctime(time.localtime(ep_time_stamp+(int(rec[0])*int(tf)))))
+      time_stamp = time.strftime("%d%m%y-%w-%H:%M:%S",(time.localtime(ep_time_stamp+(int(rec[0])*int(tf)))))
       rec[0] = time_stamp
       #print(rec)
       histData.append(rec)
 
-  print(histData)
+def saveToFile(data):
+  with open('BHARTIARTL_5min_5days.csv','w') as histFile:
+    wr = csv.writer(histFile, quoting=csv.QUOTE_ALL)
+    wr.writerow(data)
+
+
+
     
-
-  
-
 
 getHistData(300,5,'BHARTIARTL')
 
+print(histData)
+saveToFile(histData)
 
-#############
